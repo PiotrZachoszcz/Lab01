c2c0e1da9688ce0fe1ddddeb5ceb02cfe7105b08
MateuszK0808 <mateuszk0808@interia.pl>
2017-12-08 19:18:39 +0100

    raczej poprawione

--- PiotrZachoszcz1.cs
+++ PiotrZachoszcz1.cs
@@ -100,35 +100,7 @@ namespace testing
 
 	}//............................
 
-    class Tester //>>>> Klasa TESTUJACA <<<< ....................................................................................................
-    {
-        
-        static void Main1(string[] args)
-        {
-              Console.WriteLine("Zadanie 1- Sprawdzenie.............................................................. ");
-              Samochod s1 = new Samochod();
-		      s1.WypiszInfo();
-		      s1.Marka = "Fiat";
-		      s1.Model = "126p";
-		      s1.IloscDrzwi = 2;
-		      s1.PojemnoscSilnika = 650;
-		      s1.SrednieSpalanie = 6.0;
-		      s1.WypiszInfo();
-		      Samochod s2 = new Samochod("Syrena", "105", 2, 800, 7.6);
-		      s2.WypiszInfo();
-		      double kosztPrzejazdu = s2.ObliczKosztPrzejazdu(30.5, 4.85);
-		      Console.WriteLine("Koszt przejazdu: " + kosztPrzejazdu);
-		      Samochod.WypiszIloscSamochodow();
-              Console.WriteLine("Zadanie 1- Koniec Sprawdzenia.............................................................. ");
-              Console.ReadKey();
 
-
-
-
-
-        }
-
-    }  // >>>> Koniec Klasy Testującej <<<< ......................................................................
 	public class Garaz
 	{
 		private string adres;
@@ -211,8 +183,31 @@ namespace testing
 
 	class Tester2 //>>>> Klasa TESTUJACA2 <<<< ....................................................................
 	{
+		static void Main(string[] args) {
+			Zadanie1();
+			Zadanie2();
+		}
+		static void Zadanie1()
+        {
+              Console.WriteLine("Zadanie 1- Sprawdzenie.............................................................. ");
+              Samochod s1 = new Samochod();
+		      s1.WypiszInfo();
+		      s1.Marka = "Fiat";
+		      s1.Model = "126p";
+		      s1.IloscDrzwi = 2;
+		      s1.PojemnoscSilnika = 650;
+		      s1.SrednieSpalanie = 6.0;
+		      s1.WypiszInfo();
+		      Samochod s2 = new Samochod("Syrena", "105", 2, 800, 7.6);
+		      s2.WypiszInfo();
+		      double kosztPrzejazdu = s2.ObliczKosztPrzejazdu(30.5, 4.85);
+		      Console.WriteLine("Koszt przejazdu: " + kosztPrzejazdu);
+		      Samochod.WypiszIloscSamochodow();
+              Console.WriteLine("Zadanie 1- Koniec Sprawdzenia.............................................................. ");
+              Console.ReadKey();
+    	}  // >>>> Koniec Klasy Testującej <<<< ......................................................................
 
-		static void Main(string[] args)
+		static void Zadanie2()
 		{
             Console.WriteLine("Zadanie 2- Sprawdzenie..............................................................");
 			Samochod s1 = new Samochod("Fiat", "126p", 2, 650, 6.0);
